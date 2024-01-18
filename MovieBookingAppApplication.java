7f2eec5b0276dd2467f967e53ee6f394f91c742d
vikas0105 <153487391+vikas0105@users.noreply.github.com>
2024-01-18 16:04:59 +0530

    Add files via upload

---
+++ MovieBookingAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.moviebookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieBookingAppApplication.class, args);
+    }
+}
